47af3972f03499239d805537d28c526cc230e30e
ArnaldoGB <arnaldoneto151@gmail.com>
2025-01-20 15:50:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 
 # Adicionar dados da API SmoothieFroot
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.subheader(ingredients_list + 'Nutritio Information')
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredients_list)
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Seleção de ingredientes
