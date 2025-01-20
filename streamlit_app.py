ea81f6bce1809e591ab6cda224f345bac5e7f01e
ArnaldoGB <arnaldoneto151@gmail.com>
2025-01-20 15:24:22 -0300

    Update streamlit_app.py
    We need to bring in a Python package library called requests.  The requests library allows us to build and sent REST API calls.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 # Título do app
 st.title("Customize Your Smoothie!:cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
